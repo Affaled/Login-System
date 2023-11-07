f536bbd5d9e204335a6e49bad7252726167bc708
Affaled <120531449+Affaled@users.noreply.github.com>
2023-11-07 02:42:12 -0300

    remoção do teste

--- src/connection.php
+++ src/connection.php
@@ -5,6 +5,3 @@ $password = '';
 $database = 'mysql-course';
 
 $conn = new mysqli($host, $user, $password, $database);
-if ($conn->connect_errno) {
-  echo "conexão falhou (" . $conn->connect_errno . ") " . $conn->connect_error;
-}
