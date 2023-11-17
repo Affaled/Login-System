539f087a46e9a9d035747a9a95f61d13fdde6095
Affaled <120531449+Affaled@users.noreply.github.com>
2023-11-17 20:30:45 -0300

    adição de métodos de segurança na criação de user

--- src/register.php
+++ src/register.php
@@ -3,14 +3,14 @@ include 'connection.php';
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
 
-  $username = $_POST['username'];
-  $password = $_POST['password'];
+  $username = $conn->real_escape_string($_POST['username']);
+  $password = $conn->real_escape_string($_POST['password']);
 
   switch (true) {
-    case $username == '':
+    case empty(trim($username)):
       echo "Username cannot be empty";
       exit;
-    case $password == '':
+    case empty(trim($password)):
       echo "Password cannot be empty";
       exit;
     case $password != $_POST['confirm_password']:
