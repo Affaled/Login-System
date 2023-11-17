f303d9afa9e6e5c70df9d65b7909bf1fc1b3d7c8
Affaled <120531449+Affaled@users.noreply.github.com>
2023-11-17 20:18:20 -0300

    troca de ifs para switch

--- src/register.php
+++ src/register.php
@@ -6,21 +6,23 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
   $username = $_POST['username'];
   $password = $_POST['password'];
 
-  if ($_POST['password'] != $_POST['confirm_password']) {
-    echo "Passwords do not match";
-    exit;
-  } elseif ($_POST['username'] == '') {
-    echo "Username cannot be empty";
-    exit;
-  } elseif ($_POST['password'] == '') {
-    echo "Password cannot be empty";
-    exit;
-  } else {
-    $sql = "INSERT INTO useraccount (username, password) VALUES ('$username', '$password')";
-    $result = mysqli_query($conn, $sql);
+  switch (true) {
+    case $username == '':
+      echo "Username cannot be empty";
+      exit;
+    case $password == '':
+      echo "Password cannot be empty";
+      exit;
+    case $password != $_POST['confirm_password']:
+      echo "Passwords do not match";
+      exit;
+    default:
+      $sql = "INSERT INTO useraccount (username, password) VALUES ('$username', '$password')";
+      $result = mysqli_query($conn, $sql);
 
-    if ($result) {
-      header("Location: index.php");
-    }
+      if ($result) {
+        header("Location: index.php");
+      }
+      exit;
   }
 }
