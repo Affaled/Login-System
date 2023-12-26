e67a7b53fe47d65160d80106554468ccffd39bba
Lucas Affaled <affaledl@gmail.com>
2023-12-26 16:02:17 -0300

    Update register.php

--- public_html/src/register.php
+++ public_html/src/register.php
@@ -3,14 +3,14 @@ require_once 'open_database_connection.php';
 require_once 'register_validation.php';
 
 if (isset($_POST['username']) && isset($_POST['password'])) {
-
+  
+  $conn = open_database_connection();
   $username = trim($conn->real_escape_string($_POST['username']));
   $password = trim($conn->real_escape_string($_POST['password']));
   $confirm_password = trim($conn->real_escape_string($_POST['confirm_password']));
 
   $response_validation = register_validation($username, $password, $confirm_password);
   if ($response_validation["ok"]) {
-    $conn = open_database_connection();
 
     $sql = "INSERT INTO useraccount (username, password) VALUES ('$username', '$password')";
     $result = mysqli_query($conn, $sql);
